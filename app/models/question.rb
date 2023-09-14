76fd53df99d79df2ae492086d6e4b03afa1d6aa9
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-14 13:21:23 -0600

    added model validations for Questions.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,24 @@
 class Question < ApplicationRecord
+    validates(:title, presence: true)
+
+    validates(:body, presence: {message: "must be given"}, length: {minimum: 10, maximum: 2048})
+
+    validates(:view_count,numericality: {great_than_or_equal_to: 0})
+
+    validate :no_monkey
+
+    before_validation :set_default_view_count
+
+    private 
+    def set_default_view_count
+        self.view_count ||= 0
+    end
+
+    def no_monkey
+        if body&.downcase&.include?("monkey")
+            errors.add(:body,"must not have a monkey")
+        end
+    end
 end
+
+
---
+++ db/migrate/20230914124058_remove_like_count_from_questions.rb
@@ -0,0 +1,4 @@
+class RemoveLikeCountFromQuestions < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
