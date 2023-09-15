bd8c2c2c7b7890b78f74766c41032321fd5e62af
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-15 12:32:18 -0600

    migrated answer schema to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_14_124058) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_15_182237) do
+  create_table "answers", force: :cascade do |t|
+    t.text "body"
+    t.integer "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -20,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_14_124058) do
     t.integer "like_count"
   end
 
+  add_foreign_key "answers", "questions"
 end
