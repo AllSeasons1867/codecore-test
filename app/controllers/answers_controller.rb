4614a2735de62ee88b02459dbe4e145011deec19
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-15 21:14:10 -0600

    implemented create action for answers in answers controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,15 @@
 class AnswersController < ApplicationController
+    def create 
+        @question = Question.find params[:question_id]
+        @answer = Answer.new answer_params
+        @answer.question = @question 
+
+        @answer.save 
+        redirect_to question_path(@question)
+    end    
+
+    private 
+    def answer_params
+        params.require(:answer).permit(:body)
+    end
 end
