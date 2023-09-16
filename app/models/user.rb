ab8140144abcf3695e8cf9f70043906eae136975
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-16 12:44:15 -0600

    enforced validations on user creations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :first_name, :last_name, presence: true 
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+    validates(:email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX)
 end
+
+
