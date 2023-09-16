a1a9da4d7ad9defd28a39ee0e49ecd8295b201cc
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-16 13:34:31 -0600

    implemented session logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
         @user = User.new user_params
 
         if @user.save 
-            redirect_to home_path
+            session[:user_id] = @user.id
+            redirect_to users_path
         else 
             render :new
         end
