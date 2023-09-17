8e7e858069cc37f947edcbeb9eb41261e303b947
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-17 12:02:26 -0600

    implemented authentication as a before_action for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
     before_action :find_question, only: [:show, :edit, :update, :destroy]
 
     def new 
