569d8fefcfc3ec44484c60951d0e9465f015e498
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-17 00:01:13 -0600

    added user model dependecies

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
     validates(:email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX)
+
+    has_many :questions, dependent: :nullify 
+    has_many :answers, dependent: :nullify 
 end
 
 
