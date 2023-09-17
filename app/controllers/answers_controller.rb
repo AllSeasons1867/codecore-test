7a468f3f08de07b96d8cb0f85513d63f3006e0ba
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-17 12:04:41 -0600

    implemented check for whether or not user is signed in

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,6 @@
 class AnswersController < ApplicationController
+    before_action :authenticate_user!
+
     def create 
         @question = Question.find params[:question_id]
         @answer = Answer.new answer_params
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,11 @@ class ApplicationController < ActionController::Base
         end
     end
 
+    def user_signed_in? 
+        current_user.present?
+    end
+    helper_method :user_signed_in?
+
     def current_user 
         if session[:user_id].present?
             @current_user ||= User.find_by(id: session[:user_id])
