e63d2649c818973d0ce4c5293a15c08e52850aa3
FW <35238548+AllSeasons1867@users.noreply.github.com>
2023-09-17 11:54:00 -0600

    created authentication method in super controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
     private 
+    def authenticate_user! 
+        unless current_user.present? 
+            flash[:danger] = "You must sign in or sign up first!"
+            redirect_to new_session_path
+        end
+    end
+
     def current_user 
         if session[:user_id].present?
             @current_user ||= User.find_by(id: session[:user_id])
